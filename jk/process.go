57b860e5052d074e4b39bd7f267750d5a608e970
qingtao <wqt.acc@gmail.com>
2012-06-13 18:08:30 +0800

    20120613

--- jk/process.go
+++ jk/process.go
@@ -1,8 +1,7 @@
-package main
+package jk
 
 import (
     "os/exec"
-    "fmt"
 )
 
 type Process struct {
@@ -10,12 +9,13 @@ type Process struct {
     Pid string
 }
 func (p Process) String() string {
-    return fmt.Sprintf("%s: %s",p.Name,p.Pid)
+    s := p.Name + ": " + p.pid
+    return s
 }
 //var Ps []string
 var ExecPath = "/home/link0x/src/mana/jk/custom/"
 
-func (p *Process) Get() {
+func (p *Process) GetPid() {
     cmd :=  ExecPath + p.Name
     out,err := exec.Command(cmd).Output()
     var pid string
@@ -25,10 +25,4 @@ func (p *Process) Get() {
         pid = string(out)
     }
     p.Pid = pid
-}
-
-func main() {
-    var p = Process{Name:"nginx_master"}
-    p.Get()
-    fmt.Println(p)
 }
--- jk/t_temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in `awk '/°C/{print $3}' sensors.txt |sed -nr \
-    's/\+(\w+)\.\w°C/\1/p'`; do
-    if [ $i -gt 47 ]; then
-        echo Y
-        break
-    fi
-done
